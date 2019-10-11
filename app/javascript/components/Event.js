704719ed5465f1051d483afe4ccd4bec005b2c9d
HanyMoh <hanymoh@msn.com>
2019-10-11 21:48:32 +0200

    Adding Some Styling

--- app/javascript/components/Event.js
+++ app/javascript/components/Event.js
@@ -2,44 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Event = ({ event }) => (
-  <div>
-    <h2>
-      {event.event_date}
-      {' - '}
-      {event.event_type}
-    </h2>
-    <ul>
-      <li>
-        <strong>Type:</strong>
-        {' '}
-        {event.event_type}
-      </li>
-      <li>
-        <strong>Date:</strong>
-        {' '}
+
+  <div className="eventContainer">
+    <div>
+      <h2>
         {event.event_date}
-      </li>
-      <li>
-        <strong>Title:</strong>
-        {' '}
-        {event.title}
-      </li>
-      <li>
-        <strong>Speaker:</strong>
-        {' '}
-        {event.speaker}
-      </li>
-      <li>
-        <strong>Host:</strong>
-        {' '}
-        {event.host}
-      </li>
-      <li>
-        <strong>Published:</strong>
-        {' '}
-        {event.published ? 'yes' : 'no'}
-      </li>
-    </ul>
+        {' - '}
+        {event.event_type}
+      </h2>
+      <ul>
+        <li>
+          <strong>Type:</strong>
+          {' '}
+          {event.event_type}
+        </li>
+        <li>
+          <strong>Date:</strong>
+          {' '}
+          {event.event_date}
+        </li>
+        <li>
+          <strong>Title:</strong>
+          {' '}
+          {event.title}
+        </li>
+        <li>
+          <strong>Speaker:</strong>
+          {' '}
+          {event.speaker}
+        </li>
+        <li>
+          <strong>Host:</strong>
+          {' '}
+          {event.host}
+        </li>
+        <li>
+          <strong>Published:</strong>
+          {' '}
+          {event.published ? 'yes' : 'no'}
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
